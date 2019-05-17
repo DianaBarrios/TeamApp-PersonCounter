6c696f87b4e2f7d9dd14b034293c79e39811aa39
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-05-17 14:51:50 +0200

    name, team, HMW

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Hello, world!</h1>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <div class="container my-4 mx-5">
+          <p class="font-italic">How might we help companies to find a social program that matches their employee goals with their own?</p>
+          </div>
+          <h1 class="text-monospace-bold text-warning">Smiles AI</h1>
+        </header>
+      </div>
+
+      <div class="container my-5">
+        <div class="card-deck">
+
+          <div class="card">
+            <img class="card-img-top" src="https://www.xing.com/image/c_2_b_d6d996c21_22325856_4/thomas-stanner-foto.256x256.jpg" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Thomas Stanner</h5>
+              <p class="card-text">Project Manager</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <img class="card-img-top" src="https://avatars2.githubusercontent.com/u/4184874?s=460&v=4" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Luis Kentzler</h5>
+              <p class="card-text">Project Manager</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <img class="card-img-top" src="https://uploads-ssl.webflow.com/5abdd7517b8eed0b0cd96ac2/5c1dd5fcfa9b3ba74bd75961_IMG_9465%20%E6%8B%B7%E8%B2%9D-p-800.jpeg" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Sunny Chen</h5>
+              <p class="card-text">Interaction Designer</p>
+            </div>
+          </div>
+
+          <div class="card">
+            <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/21281689?s=460&v=4" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Diana Barrios</h5>
+              <p class="card-text">Software Engineer</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
-}
+} 
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +14,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//registerServiceWorker();
