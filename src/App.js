8ea1cad6dbbdada5e1b39ddae0b7431ed6647ee9
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-05-22 11:50:09 +0200

    test change for trigger 2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="App-header">
             <div class="row justify-content-center">
               <div class="WE-ARE col-6">
-                We Are
+                We Are.
               </div>
             </div>
 
