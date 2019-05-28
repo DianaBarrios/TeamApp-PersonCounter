2aa64e49ef56d9c169db416d31ef4c8eaf5c32b7
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-05-28 09:55:30 +0200

    add new page that shows last photo

---
+++ src/challenge.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+
+class Challenge extends React.Component {
+    render() {
+        return (
+            <div>
+                <div class="container ml-5">
+                    <header className="Challenge-header">
+                        <h1 class="font-weight-bold">Engineering Challenge 2</h1>
+                    </header>
+                </div>
+
+                <div class="container my-5">
+                    <img src="https://us-central1-core-228912.cloudfunctions.net/raspberry-pi-a7?apiKey=lyubomir" class="img-fluid" alt="Responsive image"></img>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Challenge
--- src/index.js
+++ src/index.js
@@ -5,10 +5,39 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { 
+  Route, 
+  NavLink, 
+  BrowserRouter as Router, 
+  Switch 
+} from 'react-router-dom'
 import App from './App';
+import Challenge from './challenge'
+import Notfound from './notfound'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/challenge">Challenge</NavLink>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/challenge" component={Challenge} />
+          <Route component={Notfound} />
+        </Switch>
+      </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
