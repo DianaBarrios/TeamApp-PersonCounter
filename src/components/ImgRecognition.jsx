26865442ebc71df7fe7072e87871945b4a2b02c5
Diana Barrios <dianabarriosmacias@hotmail.com>
2019-06-14 08:53:45 +0200

    try to download image from storage

--- src/components/ImgRecognition.jsx
+++ src/components/ImgRecognition.jsx
@@ -1,62 +1,81 @@
-import React from 'react';
+import React from "react";
 //import firebase from '../firebase.js'
-const firebase = require('../firebase.js');
+const firebase = require("../firebase.js");
 const db = firebase.db;
-const st = firebase.storage;
+const st = firebase.st;
 
 //const db = firebase.firestore();
 //const st = firebase.storage();
 
 class ImgRecognition extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      time: "",
+      date: "",
+      name: ""
+    };
+    this.watchData = this.watchData.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            image: '',
-            date: '',
-            time: '',
-            count: 0,
-        }
-        this.watchData = this.watchData.bind(this);
-    }
+  watchData() {
+    let me = this;
+    let query = db
+      .collection("PersonsCount")
+      .orderBy("timestamp", "desc")
+      .limit(1);
+    let observer = query.onSnapshot(
+      querySnapshot => {
+        // console.log(`Received query snapshot of size ${querySnapshot.size}`);
 
-   watchData(){
-        let query = db.collection('PersonsCount').orderBy('timestamp', "desc");
-        console.log({ query });
-        let observer = query.onSnapshot(querySnapshot => {
-            console.log(`Received query snapshot of size ${querySnapshot.size}`);
-            
-            var pics = [];
-
-            querySnapshot.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
-                pics.push(doc.data().count);
-              });
-          
-        }, err => {
-            console.log(`Encountered error: ${err}`);
+        let data = querySnapshot.docs[0].data();
+        // console.log(data);
+ 
+        
+        var stRef = st.ref();
+        var picsRef = stRef.child('smiles-ai-images');
+        //picsRef.getDownloadURL();
+        var fileName = data.name;
+        var picRef = picsRef.child(name);
+        picRef.getDownloadURL().then(function(url) {
+          console.log(url);
         });
-    }
-
-    
-      componentDidMount(){
-        this.watchData();
-    }
-    
-  render(){
-    return (
-        <div>
-            <div className="container">
-                <h1>
-                Image Recognition:
-                </h1>
-            </div>
-
-            
-        </div>
+        
+        me.setState({
+          count: data.count,
+          time: data.timestamp.toDate().toLocaleTimeString("en-US"),
+          date: data.timestamp.toDate().toLocaleDateString("en-US"),
+          name: data.name
+        });
+      },
+      err => {
+        console.log(`Encountered error: ${err}`);
+      }
     );
   }
 
+  componentDidMount() {
+    this.watchData();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Image Recognition:</h1>
+          <h2>There are {this.state.count} persons in this picture.</h2>
+          <h3>Date: {this.state.date}</h3>
+          <h3>Time: {this.state.time}</h3>
+        </div>
+
+        <div className="container">
+          <h3>Picture: {this.state.name}</h3>
+          <img src="" class="img-fluid" alt="Responsive image" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ImgRecognition;
