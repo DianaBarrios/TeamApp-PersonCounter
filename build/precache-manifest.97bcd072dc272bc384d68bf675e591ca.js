777e04fdc5e46bea3690d7f970bd4c00fe5724d4
lustoykov <lustoykov@gmail.com>
2019-07-10 15:34:55 +0200

    Build

---
+++ build/precache-manifest.97bcd072dc272bc384d68bf675e591ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d27cd6839d9c95a9ff6731181cf408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd6800e20687d2810f7",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4811ccadbac0a281363b",
+    "url": "/static/css/main.222004b4.chunk.css"
+  },
+  {
+    "revision": "6fd6800e20687d2810f7",
+    "url": "/static/js/2.601fb95a.chunk.js"
+  },
+  {
+    "revision": "4811ccadbac0a281363b",
+    "url": "/static/js/main.1c3a3833.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
